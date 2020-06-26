2d24972f2af06a59d5a433286af05ef7c2c54520
Svetlana Brennan <svetlana.svn@gmail.com>
2020-06-26 16:26:23 -0500

    Adding more files

--- lesson_5/set_timeout_pp.js
+++ lesson_5/set_timeout_pp.js
@@ -2,11 +2,20 @@
 1. Write a JavaScript function named delayLog that loops through the numbers from 1 to 10, and logs each number after that number of seconds. It should log 1 after 1 second, 2 after 2 seconds, and so on.
 */
 
+// function delayLog() {
+//   for (let i = 1; i <= 10; i += 1) {
+//     setTimeout(function () {
+//       console.log(i);
+//     }, 1000 * i);
+//   }
+// }
+
+
 function delayLog() {
   for (let i = 1; i <= 10; i += 1) {
     setTimeout(function () {
       console.log(i);
-    }, 10000 * i);
+    }, 1000 * i);
   }
 }
 
@@ -24,21 +33,21 @@ Using var makes the loop use the same "i" variable  with each iteration because 
 
 */
 
-setTimeout(function () {   // 1
-  console.log('Once');    // 5
-}, 1000);
+// setTimeout(function () {   // 1
+//   console.log('Once');    // 5
+// }, 1000);
 
-setTimeout(function () {   // 2
-  console.log('upon');    // 7 
-}, 3000);
+// setTimeout(function () {   // 2
+//   console.log('upon');    // 7 
+// }, 3000);
 
-setTimeout(function () {   // 3
-  console.log('a');       // 6
-}, 2000);
+// setTimeout(function () {   // 3
+//   console.log('a');       // 6
+// }, 2000);
 
-setTimeout(function () {   // 4
-  console.log('time');    // 8
-}, 4000);
+// setTimeout(function () {   // 4
+//   console.log('time');    // 8
+// }, 4000);
 
 
 /*
@@ -47,29 +56,29 @@ setTimeout(function () {   // 4
 
 */
 
-setTimeout(function () { // 1
-  setTimeout(function () { // 5
-    q();
-  }, 15);
+// setTimeout(function () { // 1
+//   setTimeout(function () { // 5
+//     q();
+//   }, 15);
 
-  d(); //6 
+//   d(); //6 
 
-  setTimeout(function () { // 7
-    n();
-  }, 5);
+//   setTimeout(function () { // 7
+//     n();
+//   }, 5);
 
-  z(); //8
-}, 10);
+//   z(); //8
+// }, 10);
 
-setTimeout(function () { // 2
-  s();
-}, 20);
+// setTimeout(function () { // 2
+//   s();
+// }, 20);
 
-setTimeout(function () { // 3
-  f();
-});
+// setTimeout(function () { // 3
+//   f();
+// });
 
-g(); // 4
+// g(); // 4
 
 // order: g, f, d, z, n, s, q
 
@@ -78,8 +87,8 @@ g(); // 4
 
 */
 
-function afterSeconds(callback, seconds) {
-  setTimeout(callback, seconds * 1000);
-}
+// function afterSeconds(callback, seconds) {
+//   setTimeout(callback, seconds * 1000);
+// }
 
-afterSeconds(() => console.log("hi"), 5);
+// afterSeconds(() => console.log("hi"), 5);
